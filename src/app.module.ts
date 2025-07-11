ea08bb3e3f02187f072914da0ecc07f7ccc47c32
Lindomar Oliveira <lindomar.souza1999@gmail.com>
2025-07-11 19:13:46 -0300

    feat: config from environment variables with validation

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { z } from 'zod';
 
 import { AppController } from '~/app.controller';
 import { AppService } from '~/app.service';
 
+const envSchema = z.object({});
+
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env.${process.env.NODE_ENV || 'development'}`,
+      validate: (env) => envSchema.parse(env)
+    })
+  ],
   controllers: [AppController],
   providers: [AppService]
 })
