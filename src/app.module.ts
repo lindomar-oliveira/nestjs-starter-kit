3a52d34578ffb1efc42c727e680eef84b10e901a
Lindomar Oliveira <lindomar.souza1999@gmail.com>
2025-07-11 19:50:54 -0300

    feat: integrate TypeORM for database access

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,19 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule, type TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { z } from 'zod';
 
 import { AppController } from '~/app.controller';
 import { AppService } from '~/app.service';
 
-const envSchema = z.object({});
+const envSchema = z.object({
+  DB_CONNECTION: z.string().nonempty(),
+  DB_HOST: z.string().optional(),
+  DB_PORT: z.coerce.number().optional(),
+  DB_USERNAME: z.string().optional(),
+  DB_PASSWORD: z.string().optional(),
+  DB_DATABASE: z.string()
+});
 
 @Module({
   imports: [
@@ -13,6 +21,19 @@ const envSchema = z.object({});
       isGlobal: true,
       envFilePath: `.env.${process.env.NODE_ENV || 'development'}`,
       validate: (env) => envSchema.parse(env)
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) =>
+        ({
+          type: configService.get('DB_CONNECTION'),
+          host: configService.get('DB_HOST'),
+          port: configService.get('DB_PORT'),
+          username: configService.get('DB_USERNAME'),
+          password: configService.get('DB_PASSWORD'),
+          database: configService.get('DB_DATABASE')
+        }) as TypeOrmModuleOptions
     })
   ],
   controllers: [AppController],
